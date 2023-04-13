e9177d1194f4e2954412c8e50de0e4b75d251f76
Shantanu Kapse <shantanu.sk05@gmail.com>
2023-04-14 02:00:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import {useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    const [state,setState] = useState('');
+    const fetchAd=()=>{
+        axios.get('https://api.adviceslip.com/advice')
+        .then((res)=>{
+            // console.log(res);
+            setState(res.data.slip.advice);
+            console.log(state);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+
+    }
+    useEffect(()=>{
+        fetchAd();
+    },[] )
+    return(
+        <>
+           
+            <div className="app">
+                <div className="header">
+                    <h1>I K I G A I</h1>
+                </div>
+                <div className="box"><i className="fas fa-quote-left fa2"></i>
+                <div className="text"><i className="fas fa-quote-right fa1"></i>
+                    <div className="head">
+                        {/* <h3>Quote the day</h3> */}
+                        <p>{state}</p>
+                    </div>
+                </div>
+                </div>
+                <button className="button-30" onClick={fetchAd}>
+                        <span>Give me Advice</span>
+                </button>
+                <div className="foot">
+                    <h3>Made with ❤ by Raycurve</h3>
+                </div>
+            </div>
+            
+        </>
+       
+    )
 }
-
-export default App;
